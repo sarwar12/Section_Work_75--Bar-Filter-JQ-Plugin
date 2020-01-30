0e7ae5548be3d062df8e41910d89b61df7e7b1f8
Golam Sarwar <32290268+sarwar12@users.noreply.github.com>
2020-01-30 09:45:13 +0600

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,14 @@
+$(document).ready(function($){
+	$('#bar1').barfiller({
+		barColor: '#ffffff'
+	});
+	$('#bar2').barfiller({
+		barColor: '#ffffff'
+	});
+	$('#bar3').barfiller({
+		barColor: '#ffffff'
+	});
+	$('#bar4').barfiller({
+		barColor: '#ffffff'
+	});
+});
